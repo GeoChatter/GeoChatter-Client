e5f5e68d48cd700435a00d4cf22007ce1c09a9ad
rhino <smhmmc98@gmail.com>
2022-09-18 23:56:34 +0300

    Change prebuilt scripts to postbuild, Sync submodules, Update submodule script

--- sync_submodules.sh
+++ sync_submodules.sh
@@ -5,4 +5,6 @@ git submodule update --init --recursive
 
 git submodule sync
 git submodule update --remote
+git submodule update --remote --merge
+git submodule update --remote --merge --recursive
 read -p "Press any key to exit..."
