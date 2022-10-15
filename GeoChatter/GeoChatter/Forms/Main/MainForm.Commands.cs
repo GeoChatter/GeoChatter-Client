c23e6d13e52d05b629bfc21c0737e591e9035e74
rhino <smhmmc98@gmail.com>
2022-10-15 22:11:27 +0300

    test commit

--- GeoChatter/GeoChatter/Forms/Main/MainForm.Commands.cs
+++ GeoChatter/GeoChatter/Forms/Main/MainForm.Commands.cs
@@ -346,7 +346,7 @@ namespace GeoChatter.Forms
                     WasRandom = true,
                     RandomGuessArgs = args.Arguments,
                     Layer = args.Layer,
-                    Source = args.Source 
+                    Source = args.Source
                 };
 
                 args.Bot?.FireGuessReceived(g);
