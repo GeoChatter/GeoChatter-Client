c32658ec458952efe894d8571ee30870a65f8670
NoBuddyHH <geochatter.app@gmail.com>
2022-10-16 16:37:41 +0200

    Added migration for the latest model change

---
+++ GeoChatter/GeoChatter.Core/Migrations/20221016143237_RandomGuessArgs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeoChatter.Core.Migrations
+{
+    public partial class RandomGuessArgs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RandomGuessArgs",
+                table: "Guess",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RandomGuessArgs",
+                table: "Guess");
+        }
+    }
+}
